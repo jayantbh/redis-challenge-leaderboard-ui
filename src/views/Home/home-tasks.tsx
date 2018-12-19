485f27db769bbef200e3a66ea6445f471de4426b
Jayant Bhawal <bhawal.jayant@gmail.com>
2018-12-19 21:04:07 +0530

    Poll for data every 30 seconds.

--- src/views/Home/home-tasks.tsx
+++ src/views/Home/home-tasks.tsx
@@ -5,10 +5,12 @@ import API from 'apis';
 import { store } from 'index';
 
 import {
-	HOME_FETCH_SUCCESS, HOME_FETCH_FAILURE
+	HOME_FETCH_SUCCESS, HOME_FETCH_FAILURE, HOME_FETCH_REQUEST
 } from './home-actions';
 
 export function* getData() {
+	setTimeout(() => store.dispatch(HOME_FETCH_REQUEST()), 30 * 1000);
+
 	try {
 		let data = yield get(API.JSON);
 		return yield put(HOME_FETCH_SUCCESS(data));
